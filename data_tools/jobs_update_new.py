810902c63803df0a205a45e5998f6b7d0023c9d6
olily <1135569701@qq.com>
2019-05-16 10:30:11 +0800

    job_spider_new

--- data_tools/jobs_update_new.py
+++ data_tools/jobs_update_new.py
@@ -75,7 +75,7 @@ class MyThread(threading.Thread):
     def run(self):
         while(True):
             try:
-                task = self.queue.get(block=True,timeout=300)
+                task = self.queue.get(block=True,timeout=500)
                 self.func(task)
                 # print(self.func)
                 self.queue.task_done()
@@ -104,12 +104,12 @@ class MyThreadPool():
 
 
 # 构造生产者
-def cursor_query(user_agent_list):
+def cursor_query():
     joburls_queue = Queue()
     query_num = 0
     # 首先查询全量数据
     sscursor = pymysql.cursors.SSCursor(db_urllist)
-    sscursor.execute('select code,url from jobs_get_copy1')
+    sscursor.execute('select code,url from jobs_get where status_update=0')
     while True:
         # 每次获取时会从上次游标的位置开始移动size个位置，返回size条数据
         data = sscursor.fetchone()
@@ -131,6 +131,7 @@ def cursor_query(user_agent_list):
 
 # 翻页爬取每一个岗位详细信息
 def get_job_detail(items):
+    # print(items)
     job_url = items[1]
     # browser = items[2]
     job_id = items[0]
@@ -138,99 +139,105 @@ def get_job_detail(items):
     job_html = html_paser(job_url)
     if job_html == 0:
         return 0
+    # print(job_html)
+
+    response = job_html.find(attrs={'class':'tHeader tHjob'})
+    study=work_year=workfare=jobfun=None
+    if response is not None:
+        job_div = job_html.find(attrs={'class': 'msg ltype'}).text
+        # print(job_div)
+        work_year = study = None
+        if job_div is not None:
+            job_div_list = job_div.split('|')
+            # print(job_div_list)
+            if len(job_div_list) >= 3:
+                work_year = job_div_list[1].replace('\xa0','')
+                study = job_div_list[2].replace('\xa0','')
+                # print(work_year,study)
+
+        # # 工作地址
+        # work_addr = None
+        # work_addr_tag = job_html.find(attrs={'class': 'bmsg inbox'})
+        # # print(work_addr_tag)
+        # if work_addr_tag is not None:
+        #     if work_addr_tag.p is not None:
+        #         work_addr = work_addr_tag.p.text
+        # # print(work_addr)
+
+        # 岗位描述
+        # jd = ""
+        # jd_tag = job_html.find(attrs={'class': 'bmsg job_msg inbox'})
+        # # print(jd_tag)
+        # if jd_tag is not None:
+        #     jd_content = jd_tag.contents
+        #     print(jd_tag.contents[1].text)
+        #     for item in jd_tag.contents:
+        #         print(11111,item.text)
+        #         jd += item.text + '_'
+        # jd = jd[:-1]
+        # print(jd)
+
+        # 岗位福利
+        workfare = ""
+        workfares=job_html.find_all(attrs={'class':'sp4'})
+        # print(workfares)
+        if len(workfares) !=0 or workfares is not None:
+            for welfare in workfares:
+                # print(welfare.text)
+                workfare += welfare.text+'_'
+            workfare = workfare[:-1]
+        else:
+            workfare = None
+        # print(workfare)
+
+        # 岗位职能
+        jobfun = ""
+        jobfun_fun = job_html.find(attrs={'class': 'fp'})
+        jobfun_div = jobfun_fun.findAll(attrs={'class': 'el tdn'})
+        if len(jobfun_div)!=0 or jobfun_div is not None :
+            for item in jobfun_div:
+                jobfun+=item.text+'_'
+            jobfun = jobfun[:-1]
+            jobfun = jobfun.replace('\r','').replace('\t','').replace('\n','').replace(' ','')
+
+            global num
+            job_info = [
+                study,
+                work_year,
+                workfare,
+                jobfun, job_id]
+            # print(job_info)
+
+            mylock.acquire()
+            num += 1
+            Producer.producer(jobs_queue, job_info)
+            mylock.release()
 
 
-    job_div = job_html.find(attrs={'class': 'msg ltype'}).text
-    # print(job_div)
-    work_year = study = None
-    if job_div is not None:
-        job_div_list = job_div.split('|')
-        # print(job_div_list)
-        if len(job_div_list) >= 3:
-            work_year = job_div_list[1].replace('\xa0','')
-            study = job_div_list[2].replace('\xa0','')
-            # print(work_year,study)
-
-    # # 工作地址
-    # work_addr = None
-    # work_addr_tag = job_html.find(attrs={'class': 'bmsg inbox'})
-    # # print(work_addr_tag)
-    # if work_addr_tag is not None:
-    #     if work_addr_tag.p is not None:
-    #         work_addr = work_addr_tag.p.text
-    # # print(work_addr)
-
-    # 岗位描述
-    # jd = ""
-    # jd_tag = job_html.find(attrs={'class': 'bmsg job_msg inbox'})
-    # # print(jd_tag)
-    # if jd_tag is not None:
-    #     jd_content = jd_tag.contents
-    #     print(jd_tag.contents[1].text)
-    #     for item in jd_tag.contents:
-    #         print(11111,item.text)
-    #         jd += item.text + '_'
-    # jd = jd[:-1]
-    # print(jd)
-
-    # 岗位福利
-    workfare = ""
-    workfares=job_html.find_all(attrs={'class':'sp4'})
-    # print(workfares)
-    if len(workfares) !=0 or workfares is not None:
-        for welfare in workfares:
-            # print(welfare.text)
-            workfare += welfare.text+'_'
-        workfare = workfare[:-1]
-    else:
-        workfare = None
-    # print(workfare)
-
-    # 岗位职能
-    jobfun = ""
-    jobfun_fun = job_html.find(attrs={'class': 'fp'})
-    jobfun_div = jobfun_fun.findAll(attrs={'class': 'el tdn'})
-    if len(jobfun_div)!=0 or jobfun_div is not None :
-        for item in jobfun_div:
-            jobfun+=item.text+'_'
-        jobfun = jobfun[:-1]
-        jobfun = jobfun.replace('\r','').replace('\t','').replace('\n','').replace(' ','')
-
-    global num
-    num += 1
-    job_info = [
-        study,
-        work_year,
-        workfare,
-        jobfun,job_id]
-    # print(job_info)
-    mylock.acquire()
-    num+=1
-    Producer.producer(jobs_queue, job_info)
-    mylock.release()
 
     if num % 10000 == 0:
         print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "request", num)
 
 
 def insertDB(sql_value):
+    # print("进入insertdb")
     global save_num
     db_urllist.ping(reconnect=True)
     # 提交到数据库执行,每1000条提交一次
-    sql = 'update jobs_get_copy1 set education="%s",work_year="%s",jobfare = "%s",job_fun = "%s" where code="%s"' % (str(sql_value[0]),str(sql_value[1]),str(sql_value[2]),str(sql_value[3]),str(sql_value[4]))
-    print(sql)
+    sql = 'update jobs_get set education="%s",work_year="%s",jobfare = "%s",job_fun = "%s",status_update=1 where code="%s"' % (str(sql_value[0]),str(sql_value[1]),str(sql_value[2]),str(sql_value[3]),str(sql_value[4]))
+    # print(sql)
     # SQL 插入语句
     try:
         mylock2.acquire()
         save_num += 1
         cursor_urllist.execute(sql)
         # print(save_num)
+        # db_urllist.commit()
         # print(sql)
-        db_urllist.commit()
-        # if save_num % 100 == 0:
-        #     db.commit()
-        #     if save_num % 10000 == 0:
-        #         print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),"save",save_num)
+        if save_num % 1000 == 0:
+            db_urllist.commit()
+            if save_num % 10000 == 0:
+                print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),"save",save_num)
         mylock2.release()
     except :
         mylock2.release()
@@ -267,14 +274,14 @@ f.close()
 fp = open("job_update.txt", "a+", encoding="utf-8")
 
 
-q = cursor_query(user_agent_list)
+q = cursor_query()
 
 jobs_queue = Queue()
 save_num = 0
 
 pool = MyThreadPool()
 pool.addthread(queue=q, size=18,func=get_job_detail)
-pool.addthread(queue=jobs_queue, size=5,func=insertDB)
+pool.addthread(queue=jobs_queue, size=1,func=insertDB)
 pool.startAll()
 pool.joinAll()
 
